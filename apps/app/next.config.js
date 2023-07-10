cd1374644195a8a65158f28e7b161eacc38f65df
Ncookiez <ncukiert@gmail.com>
2023-07-10 14:50:56 -0400

    API CORS Headers

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -12,6 +12,17 @@ module.exports = {
       config.resolve.fallback = { fs: false, net: false, tls: false }
     }
     return config;
+  },
+  async headers() {
+    return [{
+      source: '/api/:path*',
+      headers: [
+        { key: "Access-Control-Allow-Credentials", value: "true" },
+        { key: "Access-Control-Allow-Origin", value: "*" },
+        { key: "Access-Control-Allow-Methods", value: "GET" },
+        { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
+      ]
+    }]
   }
 };
 
