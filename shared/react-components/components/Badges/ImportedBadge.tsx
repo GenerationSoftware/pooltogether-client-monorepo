2ede1461268ad3ef8f679656e5c47270147371ef
Ncookiez <ncukiert@gmail.com>
2023-09-06 17:53:25 -0400

    Fixed Imported Badge Centering

--- shared/react-components/components/Badges/ImportedBadge.tsx
+++ shared/react-components/components/Badges/ImportedBadge.tsx
@@ -8,7 +8,7 @@ export const ImportedBadge = (props: ImportedBadgeProps) => {
   const { intl } = props
 
   return (
-    <div className='flex gap-0.5 bg-pt-warning-light text-pt-warning-dark px-3 py-0.5 rounded-[6px]'>
+    <div className='flex gap-0.5 items-center bg-pt-warning-light text-pt-warning-dark px-3 py-0.5 rounded-[6px]'>
       <ExclamationTriangleIcon className='h-4 w-4 text-inherit' />
       <span className='text-sm font-medium'>{intl?.text ?? 'Imported'}</span>
     </div>
