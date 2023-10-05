4d2ec9e6fe21b99f5a0fcecfed0ad54fb419415d
Ncookiez <ncukiert@gmail.com>
2023-10-05 17:17:15 -0400

    Added `AvgClaimFeePercentagesChart`

---
+++ apps/analytics/src/components/Charts/AvgClaimFeePercentagesChart.tsx
@@ -0,0 +1,71 @@
+import { PrizePool } from '@generationsoftware/hyperstructure-client-js'
+import { usePrizeDrawWinners } from '@generationsoftware/hyperstructure-react-hooks'
+import { divideBigInts, formatNumberForDisplay } from '@shared/utilities'
+import classNames from 'classnames'
+import { useMemo } from 'react'
+import { useDrawClosedEvents } from '@hooks/useDrawClosedEvents'
+import { LineChart } from './LineChart'
+
+interface AvgClaimFeePercentagesChartProps {
+  prizePool: PrizePool
+  hideCanary?: boolean
+  className?: string
+}
+
+export const AvgClaimFeePercentagesChart = (props: AvgClaimFeePercentagesChartProps) => {
+  const { prizePool, hideCanary, className } = props
+
+  const { data: allDraws } = usePrizeDrawWinners(prizePool)
+
+  const { data: drawClosedEvents } = useDrawClosedEvents(prizePool)
+
+  const chartData = useMemo(() => {
+    if (!!allDraws && !!drawClosedEvents) {
+      const data: { name: string; percentage: number }[] = []
+
+      allDraws.forEach((draw) => {
+        // TODO: switch to `args.numTiers` once event is fixed
+        const numTiers = drawClosedEvents?.find((e) => e.args.drawId === draw.id)?.args.nextNumTiers
+        if (!!numTiers) {
+          const wins = draw.prizeClaims.filter(
+            (win) => win.fee > 0n && (!hideCanary || win.tier !== numTiers - 1)
+          )
+
+          const sumClaimFeeAmount = wins.reduce((a, b) => a + b.fee, 0n)
+          const sumPrizeAmount = wins.reduce((a, b) => a + b.payout, 0n)
+
+          data.push({
+            name: `#${draw.id}`,
+            percentage: divideBigInts(sumClaimFeeAmount, sumPrizeAmount + sumClaimFeeAmount) * 100
+          })
+        }
+      })
+
+      return data
+    }
+  }, [allDraws, drawClosedEvents])
+
+  if (!!chartData?.length) {
+    return (
+      <div className={classNames('w-full flex flex-col gap-2', className)}>
+        <span className='ml-2 md:ml-6'>Average Claim Fee Percentages</span>
+        <LineChart
+          data={chartData}
+          lines={[{ id: 'percentage' }]}
+          tooltip={{
+            show: true,
+            formatter: (value) => [
+              `${formatNumberForDisplay(value, { maximumFractionDigits: 2 })}%`,
+              'Avg Claim Fee'
+            ],
+            labelFormatter: (label) => `Draw ${label}`
+          }}
+          xAxis={{ interval: 2 }}
+          yAxis={{ tickFormatter: (tick) => `${tick}%` }}
+        />
+      </div>
+    )
+  }
+
+  return <></>
+}
--- apps/analytics/src/views/DrawsView.tsx
+++ apps/analytics/src/views/DrawsView.tsx
@@ -1,8 +1,10 @@
 import { PrizePool } from '@generationsoftware/hyperstructure-client-js'
 import { PRIZE_POOLS, sToMs } from '@shared/utilities'
+import classNames from 'classnames'
 import { useEffect, useMemo } from 'react'
 import { Address } from 'viem'
 import { usePublicClient } from 'wagmi'
+import { AvgClaimFeePercentagesChart } from '@components/Charts/AvgClaimFeePercentagesChart'
 import { DrawCards } from '@components/Draws/DrawCards'
 import { useDrawClosedEvents } from '@hooks/useDrawClosedEvents'
 import { useDrawRngFeePercentage } from '@hooks/useDrawRngFeePercentage'
@@ -60,5 +62,12 @@ export const DrawsView = (props: DrawsViewProps) => {
     return () => clearInterval(interval)
   }, [])
 
-  return <DrawCards prizePool={prizePool} className={className} />
+  return (
+    <div className={classNames('w-full flex flex-col gap-6 items-center', className)}>
+      <div className='w-full grid grid-cols-1 gap-6 md:grid-cols-2'>
+        <AvgClaimFeePercentagesChart prizePool={prizePool} />
+      </div>
+      <DrawCards prizePool={prizePool} />
+    </div>
+  )
 }
