1bdbd5e2b242a0da8dce11ac81ea3406a81b96e2
Ncookiez <ncukiert@gmail.com>
2023-10-19 18:21:35 -0400

    Added Recent Burn Stats

--- apps/analytics/src/views/BurnView.tsx
+++ apps/analytics/src/views/BurnView.tsx
@@ -1,11 +1,17 @@
 import { PrizePool } from '@generationsoftware/hyperstructure-client-js'
-import { usePrizeTokenData } from '@generationsoftware/hyperstructure-react-hooks'
-import { PRIZE_POOLS } from '@shared/utilities'
+import {
+  useBlockAtTimestamp,
+  usePrizeTokenData
+} from '@generationsoftware/hyperstructure-react-hooks'
+import { PRIZE_POOLS, SECONDS_PER_DAY } from '@shared/utilities'
 import classNames from 'classnames'
+import { useAtomValue } from 'jotai'
 import { useMemo } from 'react'
+import { currentTimestampAtom } from 'src/atoms'
 import { Address } from 'viem'
 import { usePublicClient } from 'wagmi'
 import { BurnHeader } from '@components/Burn/BurnHeader'
+import { RecentBurnStats } from '@components/Burn/RecentBurnStats'
 import { BurnChart } from '@components/Charts/BurnChart'
 
 interface BurnViewProps {
@@ -17,6 +23,8 @@ export const BurnView = (props: BurnViewProps) => {
   const { chainId, className } = props
 
   const publicClient = usePublicClient({ chainId })
+
+  const currentTimestamp = useAtomValue(currentTimestampAtom)
 
   const prizePool = useMemo(() => {
     const prizePoolInfo = PRIZE_POOLS.find((pool) => pool.chainId === chainId) as {
@@ -35,10 +43,28 @@ export const BurnView = (props: BurnViewProps) => {
 
   const { data: prizeToken } = usePrizeTokenData(prizePool)
 
+  const { data: minBlockDay } = useBlockAtTimestamp(
+    prizePool.chainId,
+    currentTimestamp - SECONDS_PER_DAY
+  )
+
+  const { data: minBlockWeek } = useBlockAtTimestamp(
+    prizePool.chainId,
+    currentTimestamp - SECONDS_PER_DAY * 7
+  )
+
   if (!!prizeToken) {
     return (
       <div className={classNames('w-full flex flex-col gap-6 items-center', className)}>
-        <BurnHeader prizeToken={prizeToken} />
+        <div className='flex flex-col items-center'>
+          <BurnHeader prizeToken={prizeToken} className='mb-3' />
+          {!!minBlockDay && (
+            <RecentBurnStats prizeToken={prizeToken} minBlock={minBlockDay} label='Last 24 hours' />
+          )}
+          {!!minBlockWeek && (
+            <RecentBurnStats prizeToken={prizeToken} minBlock={minBlockWeek} label='Last week' />
+          )}
+        </div>
         <BurnChart prizePool={prizePool} prizeToken={prizeToken} />
       </div>
     )
