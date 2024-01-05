204141e36dd4e9dfdfd72a7cf0206ba97c3672a8
Ncookie <ncukiert@gmail.com>
2024-01-05 17:17:51 -0300

    Added POOL token logo fallbacks

--- shared/react-components/constants.ts
+++ shared/react-components/constants.ts
@@ -1,9 +1,10 @@
-import { NETWORK } from '@shared/utilities'
+import { NETWORK, POOL_TOKEN_ADDRESSES } from '@shared/utilities'
 
 /**
  * Token Logo URLs
  */
 const tokenLogoUrls = Object.freeze({
+  pool: 'https://etherscan.io/token/images/pooltogether_32.png',
   usdc: 'https://etherscan.io/token/images/centre-usdc_28.png',
   dai: 'https://assets.coingecko.com/coins/images/9956/small/4943.png?1636636734',
   gusd: 'https://assets.coingecko.com/coins/images/5992/small/gemini-dollar-gusd.png?1536745278',
@@ -20,6 +21,7 @@ const tokenLogoUrls = Object.freeze({
  */
 export const TOKEN_LOGO_OVERRIDES: Record<NETWORK, { [address: Lowercase<string>]: string }> = {
   [NETWORK.mainnet]: {
+    [POOL_TOKEN_ADDRESSES[NETWORK.mainnet].toLowerCase()]: tokenLogoUrls.pool,
     '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': tokenLogoUrls.usdc,
     '0x6b175474e89094c44da98b954eedeac495271d0f': tokenLogoUrls.dai,
     '0x056fd409e1d7a124bd7017459dfea2f387b6d5cd': tokenLogoUrls.gusd,
@@ -32,6 +34,7 @@ export const TOKEN_LOGO_OVERRIDES: Record<NETWORK, { [address: Lowercase<string>
   [NETWORK.bsc_testnet]: {},
   [NETWORK.xdai]: {},
   [NETWORK.polygon]: {
+    [POOL_TOKEN_ADDRESSES[NETWORK.polygon].toLowerCase()]: tokenLogoUrls.pool,
     '0x2791bca1f2de4661ed88a30c99a7a9449aa84174': tokenLogoUrls.usdc,
     '0x8f3cf7ad23cd3cadbd9735aff958023239c6a063': tokenLogoUrls.dai,
     '0xc8a94a3d3d2dabc3c1caffffdca6a7543c3e3e65': tokenLogoUrls.gusd,
@@ -40,6 +43,7 @@ export const TOKEN_LOGO_OVERRIDES: Record<NETWORK, { [address: Lowercase<string>
   },
   [NETWORK.mumbai]: {},
   [NETWORK.optimism]: {
+    [POOL_TOKEN_ADDRESSES[NETWORK.optimism].toLowerCase()]: tokenLogoUrls.pool,
     '0x7f5c764cbc14f9669b88837ca1490cca17c31607': tokenLogoUrls.usdc,
     '0xda10009cbd5d07dd0cecc66161fc93d7c9000da1': tokenLogoUrls.dai,
     '0x4200000000000000000000000000000000000006': tokenLogoUrls.weth,
@@ -48,6 +52,7 @@ export const TOKEN_LOGO_OVERRIDES: Record<NETWORK, { [address: Lowercase<string>
     '0x9485aca5bbbe1667ad97c7fe7c4531a624c8b1ed': tokenLogoUrls.ageur
   },
   [NETWORK.optimism_sepolia]: {
+    [POOL_TOKEN_ADDRESSES[NETWORK.optimism_sepolia].toLowerCase()]: tokenLogoUrls.pool,
     '0x8067f3cb6eef936256108ff19a05574b8ad99cf3': tokenLogoUrls.usdc,
     '0xd590ec14364731b62265a5cc807164a17c6797d4': tokenLogoUrls.dai,
     '0x1a188719711d62423abf1a4de7d8aa9014a39d73': tokenLogoUrls.gusd,
@@ -62,6 +67,7 @@ export const TOKEN_LOGO_OVERRIDES: Record<NETWORK, { [address: Lowercase<string>
   [NETWORK.celo_testnet]: {},
   [NETWORK.arbitrum]: {},
   [NETWORK.arbitrum_sepolia]: {
+    [POOL_TOKEN_ADDRESSES[NETWORK.arbitrum_sepolia].toLowerCase()]: tokenLogoUrls.pool,
     '0x7a6dbc7ff4f1a2d864291db3aec105a8eee4a3d2': tokenLogoUrls.usdc,
     '0x08c19fe57af150a1af975cb9a38769848c7df98e': tokenLogoUrls.dai,
     '0xb84460d777133a4b86540d557db35952e4adfee7': tokenLogoUrls.gusd,
