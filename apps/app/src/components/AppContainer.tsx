32f58fcf339ed3cce944e3012fdcea6b59a58a98
Charles Bergeron <chuck.bergeron@gmail.com>
2024-01-18 16:47:40 -0800

    attempting to prevent the flicker of English to whatever the previously chosen language is for a user

--- apps/app/src/components/AppContainer.tsx
+++ apps/app/src/components/AppContainer.tsx
@@ -1,10 +1,10 @@
-import { useSelectedLanguage } from '@shared/generic-react-hooks'
+import { useLocalStorageLanguage, useSelectedLanguage } from '@shared/generic-react-hooks'
 import { Flowbite } from '@shared/ui'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { NextIntlProvider } from 'next-intl'
 import { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 // React Query Client:
 const queryClient = new QueryClient()
@@ -12,12 +12,22 @@ const queryClient = new QueryClient()
 export const AppContainer = (props: AppProps) => {
   const { Component, pageProps } = props
 
+  const [ready, setReady] = useState<boolean>(false)
+
   const router = useRouter()
 
   useSelectedLanguage({
     onLanguageChange: (locale) => {
       const { pathname, query, asPath } = router
       router.push({ pathname, query }, asPath, { locale })
+    }
+  })
+
+  useLocalStorageLanguage((lang: string | undefined) => {
+    console.log('lang')
+    console.log(lang)
+    if (!ready && lang) {
+      setReady(true)
     }
   })
 
@@ -32,7 +42,7 @@ export const AppContainer = (props: AppProps) => {
       <QueryClientProvider client={queryClient}>
         <NextIntlProvider messages={pageProps.messages}>
           <div id='modal-root' />
-          <Component {...pageProps} />
+          {ready ? <Component {...pageProps} /> : null}
         </NextIntlProvider>
       </QueryClientProvider>
     </Flowbite>
