f82dcc672707af7e9bb87623418fbdc580fa0f9a
Ncookiez <ncukiert@gmail.com>
2024-02-07 18:24:31 -0500

    Resolved conflict on SimpleInput used on DelegateForm

--- shared/react-components/components/Form/DelegateForm.tsx
+++ shared/react-components/components/Form/DelegateForm.tsx
@@ -3,15 +3,15 @@ import { useUserVaultDelegate } from '@generationsoftware/hyperstructure-react-h
 import { PencilIcon } from '@heroicons/react/24/outline'
 import { Intl } from '@shared/types'
 import { Spinner } from '@shared/ui'
+import classNames from 'classnames'
 import { atom, useSetAtom } from 'jotai'
-import { useEffect } from 'react'
+import { ReactNode, useEffect } from 'react'
 import { useState } from 'react'
-import { FormProvider, useForm } from 'react-hook-form'
+import { FormProvider, useForm, useFormContext, useWatch } from 'react-hook-form'
 import { Address, isAddress } from 'viem'
 import { useAccount } from 'wagmi'
 import { DelegateModalView } from '../Modals/DelegateModal'
 import { DelegationDescriptionTooltip } from '../Tooltips/DelegationDescriptionTooltip'
-import { SimpleInput } from './SimpleInput'
 
 export const delegateFormNewDelegateAddressAtom = atom<Address | undefined>('0x')
 
@@ -67,7 +67,7 @@ export const DelegateForm = (props: DelegateFormProps) => {
   return (
     <div className='flex flex-col'>
       <FormProvider {...formMethods}>
-        <SimpleInput
+        <DelegateInput
           formKey='newDelegateAddress'
           autoComplete='off'
           disabled={disabled}
@@ -107,6 +107,115 @@ export const DelegateForm = (props: DelegateFormProps) => {
           className='w-full max-w-md'
         />
       </FormProvider>
+    </div>
+  )
+}
+
+interface DelegateInputProps {
+  formKey: string
+  id?: string
+  autoComplete?: string
+  validate?: { [rule: string]: (v: any) => true | string }
+  placeholder?: string
+  defaultValue?: string
+  label?: ReactNode
+  hideErrorMsgs?: boolean
+  autoFocus?: boolean
+  disabled?: boolean
+  needsOverride?: boolean
+  isActiveOverride?: boolean
+  setIsActiveOverride?: (val: boolean) => void
+  overrideLabel?: ReactNode
+  keepValueOnOverride?: boolean
+  onOverride?: (val: boolean) => void
+  className?: string
+  labelClassName?: string
+  innerClassName?: string
+  errorClassName?: string
+}
+
+export const DelegateInput = (props: DelegateInputProps) => {
+  const {
+    formKey,
+    id,
+    autoComplete,
+    validate,
+    placeholder,
+    defaultValue,
+    label,
+    hideErrorMsgs,
+    autoFocus,
+    disabled,
+    needsOverride,
+    setIsActiveOverride,
+    isActiveOverride,
+    overrideLabel,
+    keepValueOnOverride,
+    onOverride,
+    className,
+    labelClassName,
+    innerClassName,
+    errorClassName
+  } = props
+
+  const { register, formState, setValue } = useFormContext()
+
+  const formValues = useWatch()
+
+  const handleOverride = () => {
+    !keepValueOnOverride && setValue(formKey, '')
+    setIsActiveOverride?.(true)
+    onOverride?.(true)
+  }
+
+  const handleBlur = () => {
+    if ((needsOverride && !formValues[formKey]) || formValues[formKey] === defaultValue) {
+      setValue(formKey, defaultValue, { shouldValidate: true })
+      setIsActiveOverride?.(false)
+      onOverride?.(false)
+    }
+  }
+
+  const error = formState.errors[formKey]?.message as string | undefined
+
+  return (
+    <div className={classNames('flex flex-col gap-2', className)}>
+      <label
+        htmlFor={id ?? formKey}
+        className={classNames('flex items-center justify-between text-sm', labelClassName)}
+      >
+        <span className='font-medium text-pt-purple-100'>{label}</span>
+        {needsOverride && !isActiveOverride && (
+          <span onClick={handleOverride} className='text-pt-teal cursor-pointer underline'>
+            {overrideLabel ?? 'override'}
+          </span>
+        )}
+      </label>
+      <input
+        id={id ?? formKey}
+        {...register(formKey, { validate })}
+        placeholder={placeholder}
+        defaultValue={defaultValue}
+        autoFocus={autoFocus}
+        onBlur={handleBlur}
+        autoComplete={autoComplete}
+        onClick={handleOverride}
+        className={classNames(
+          'px-3 py-2 text-sm leading-tight rounded-lg border outline outline-1',
+          'md:px-4 md:py-3',
+          {
+            'bg-pt-purple-50 text-gray-700 border-gray-300':
+              !needsOverride || (needsOverride && isActiveOverride),
+            'bg-transparent text-pt-teal border-pt-teal cursor-pointer':
+              needsOverride && !isActiveOverride,
+            'brightness-75': disabled,
+            [`outline-red-600 ${errorClassName}`]: !!error,
+            'outline-transparent': !error
+          },
+          innerClassName
+        )}
+      />
+      {!hideErrorMsgs && <span className='text-xs text-pt-warning-light'>{error}</span>}
     </div>
   )
 }
