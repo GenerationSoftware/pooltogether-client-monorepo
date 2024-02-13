e8ac4167ee0a60901da70d522a7ff59438b613f2
Ncookiez <ncukiert@gmail.com>
2024-02-13 11:21:30 -0500

    freeze -> const

--- shared/generic-react-hooks/constants/query.ts
+++ shared/generic-react-hooks/constants/query.ts
@@ -1,10 +1,10 @@
 /**
  * Basic config to avoid refetching data
  */
-export const NO_REFETCH = Object.freeze({
+export const NO_REFETCH = {
   refetchInterval: false,
   refetchIntervalInBackground: false,
   refetchOnMount: false,
   refetchOnReconnect: false,
   refetchOnWindowFocus: false
-})
+} as const
