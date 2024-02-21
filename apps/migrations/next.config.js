414940df2f49542214ba717027f5ea80f037e388
Ncookiez <ncukiert@gmail.com>
2024-02-21 12:23:04 -0500

    resolving some migration app build errors

--- apps/migrations/next.config.js
+++ apps/migrations/next.config.js
@@ -13,6 +13,7 @@ module.exports = {
     if (!isServer) {
       config.resolve.fallback = { fs: false, net: false, tls: false }
     }
+    config.externals.push('pino-pretty', 'encoding')
     return config
   }
 }
