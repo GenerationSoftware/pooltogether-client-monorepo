2476a7016fa96e72907db23a1e3565b58b88eaf4
Ncookiez <ncukiert@gmail.com>
2024-03-19 16:52:35 -0400

    refactored extension pages

--- apps/incentives/src/pages/on-ramps.tsx
+++ apps/incentives/src/pages/on-ramps.tsx
@@ -1,66 +1,59 @@
 import { LINKS } from '@shared/ui'
+import { ExtensionsPageContent } from '@components/ExtensionsPageContent'
 import { Layout } from '@components/Layout'
-import { ResourceLink } from '@components/ResourceLink'
-import { SimpleSection } from '@components/SimpleSection'
 
 export default function OnRampsPage() {
   return (
     <Layout className='gap-6'>
-      <h1 className='w-full text-5xl'>Web 2.5 On-Ramps</h1>
-      <hr className='w-full border-pt-purple-300' />
-      <div className='w-full grid grid-cols-1 gap-x-4 gap-y-6 md:grid-cols-2'>
-        <SimpleSection
-          title={`What's the idea?`}
-          content={
-            <>
-              <p>
-                One of the biggest challenges users face in using DeFi protocols like PoolTogether
-                is the onboarding phase of wallet management and funding. Tools like
-                smart/abstracted wallets and simple fiat-to-crypto on-ramps can alleviate some of
-                this friction.
-              </p>
-              <p>
-                We welcome any builders to build new front-ends that abstract out these challenges
-                to new PoolTogether users. We can provide you with tooling and help through our many
-                social channels, as well as funding if necessary. Come chat with us!
-              </p>
-            </>
+      <ExtensionsPageContent
+        title='Web 2.5 On-Ramps'
+        idea={
+          <>
+            <p>
+              One of the biggest challenges users face in using DeFi protocols like PoolTogether is
+              the onboarding phase of wallet management and funding. Tools like smart/abstracted
+              wallets and simple fiat-to-crypto on-ramps can alleviate some of this friction.
+            </p>
+            <p>
+              We welcome any builders to build new front-ends that abstract out these challenges to
+              new PoolTogether users. We can provide you with tooling and help through our many
+              social channels, as well as funding if necessary. Come chat with us!
+            </p>
+          </>
+        }
+        communityLinks={[
+          {
+            name: 'Chat with us in #incentives',
+            href: LINKS.discord
+          },
+          {
+            name: 'Post a PTBR to request funding',
+            href: 'https://gov.pooltogether.com/c/governance/budget-requests'
           }
-        />
-        <div className='flex flex-col gap-6'>
-          <SimpleSection
-            title='Connect with the community'
-            content={
-              <div className='flex flex-col gap-4 items-start'>
-                <ResourceLink href={LINKS.discord}>Chat with us in #incentives</ResourceLink>
-                <ResourceLink href='https://gov.pooltogether.com/c/governance/budget-requests'>
-                  Post a PTBR to request funding
-                </ResourceLink>
-              </div>
-            }
-          />
-          <SimpleSection
-            title='Resources'
-            content={
-              <div className='flex flex-col gap-4 items-start'>
-                <ResourceLink href={LINKS.protocolDevDocs}>PoolTogether V5 Dev Docs</ResourceLink>
-                <ResourceLink href='https://github.com/GenerationSoftware/pooltogether-sveltekit-template'>
-                  SvelteKit App Template
-                </ResourceLink>
-                <ResourceLink href='https://github.com/GenerationSoftware/pooltogether-nextjs-template'>
-                  NextJS App Template
-                </ResourceLink>
-                <ResourceLink href='https://www.npmjs.com/package/@generationsoftware/hyperstructure-client-js'>
-                  PoolTogether Client SDK
-                </ResourceLink>
-                <ResourceLink href='https://www.npmjs.com/package/@generationsoftware/hyperstructure-react-hooks'>
-                  PoolTogether React Hooks
-                </ResourceLink>
-              </div>
-            }
-          />
-        </div>
-      </div>
+        ]}
+        resources={[
+          {
+            name: 'PoolTogether V5 Dev Docs',
+            href: LINKS.protocolDevDocs
+          },
+          {
+            name: 'SvelteKit App Template',
+            href: 'https://github.com/GenerationSoftware/pooltogether-sveltekit-template'
+          },
+          {
+            name: 'NextJS App Template',
+            href: 'https://github.com/GenerationSoftware/pooltogether-nextjs-template'
+          },
+          {
+            name: 'PoolTogether Client SDK',
+            href: 'https://www.npmjs.com/package/@generationsoftware/hyperstructure-client-js'
+          },
+          {
+            name: 'PoolTogether React Hooks',
+            href: 'https://www.npmjs.com/package/@generationsoftware/hyperstructure-react-hooks'
+          }
+        ]}
+      />
     </Layout>
   )
 }
--- apps/incentives/src/pages/yield-sources.tsx
+++ apps/incentives/src/pages/yield-sources.tsx
@@ -1,57 +1,43 @@
 import { LINKS } from '@shared/ui'
+import { ExtensionsPageContent } from '@components/ExtensionsPageContent'
 import { Layout } from '@components/Layout'
-import { ResourceLink } from '@components/ResourceLink'
-import { SimpleSection } from '@components/SimpleSection'
 
 export default function YieldSourcesPage() {
   return (
     <Layout className='gap-6'>
-      <h1 className='w-full text-5xl'>New Yield Sources</h1>
-      <hr className='w-full border-pt-purple-300' />
-      <div className='w-full grid grid-cols-1 gap-x-4 gap-y-6 md:grid-cols-2'>
-        <SimpleSection
-          title={`What's the idea?`}
-          content={
-            <>
-              <p>
-                PoolTogether relies on yield sources to generate prizes. However, to keep users
-                safe, these yield sources need due diligence and proper auditing. Furthermore, the
-                interactions between a prize vault and its underlying yield source should also be
-                examined closely.
-              </p>
-              <p>
-                We invite anyone to share yield sources to be integrated with PoolTogether V5. If
-                funding is necessary for audits and/or building contract wrappers, we encourage
-                requesting a budget through a PTBR.
-              </p>
-            </>
+      <ExtensionsPageContent
+        title='New Yield Sources'
+        idea={
+          <>
+            <p>
+              PoolTogether relies on yield sources to generate prizes. However, to keep users safe,
+              these yield sources need due diligence and proper auditing. Furthermore, the
+              interactions between a prize vault and its underlying yield source should also be
+              examined closely.
+            </p>
+            <p>
+              We invite anyone to share yield sources to be integrated with PoolTogether V5. If
+              funding is necessary for audits and/or building contract wrappers, we encourage
+              requesting a budget through a PTBR.
+            </p>
+          </>
+        }
+        communityLinks={[
+          {
+            name: 'Chat with us in #incentives',
+            href: LINKS.discord
+          },
+          {
+            name: 'Discuss yield sources in the forums',
+            href: 'https://gov.pooltogether.com/c/ideas-and-discussion'
+          },
+          {
+            name: 'Post a PTBR to request funding',
+            href: 'https://gov.pooltogether.com/c/governance/budget-requests'
           }
-        />
-        <div className='flex flex-col gap-6'>
-          <SimpleSection
-            title='Connect with the community'
-            content={
-              <div className='flex flex-col gap-4 items-start'>
-                <ResourceLink href={LINKS.discord}>Chat with us in #incentives</ResourceLink>
-                <ResourceLink href='https://gov.pooltogether.com/c/ideas-and-discussion'>
-                  Discuss yield sources in the forums
-                </ResourceLink>
-                <ResourceLink href='https://gov.pooltogether.com/c/governance/budget-requests'>
-                  Post a PTBR to request funding
-                </ResourceLink>
-              </div>
-            }
-          />
-          <SimpleSection
-            title='Resources'
-            content={
-              <div className='flex flex-col gap-4 items-start'>
-                <ResourceLink href={LINKS.protocolDevDocs}>PoolTogether V5 Dev Docs</ResourceLink>
-              </div>
-            }
-          />
-        </div>
-      </div>
+        ]}
+        resources={[{ name: 'PoolTogether V5 Dev Docs', href: LINKS.protocolDevDocs }]}
+      />
     </Layout>
   )
 }
