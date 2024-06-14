83802d0f839da19e337ea27cef5eabb1ad8b20d4
Ncookiez <ncukiert@gmail.com>
2024-06-14 12:05:51 -0400

    removed unecessary buggy protection on `usePrizeOdds`

--- packages/hyperstructure-react-hooks/src/prizes/usePrizeOdds.ts
+++ packages/hyperstructure-react-hooks/src/prizes/usePrizeOdds.ts
@@ -37,7 +37,7 @@ export const usePrizeOdds = (
         )
       : 0
 
-  const oneInX = percent !== 0 ? 1 / percent : Number.MAX_SAFE_INTEGER
+  const oneInX = 1 / percent
 
   const data = isFetched && isSuccess ? { percent, oneInX } : undefined
 
