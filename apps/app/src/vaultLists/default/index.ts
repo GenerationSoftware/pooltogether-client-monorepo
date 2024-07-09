985a102cfbc980dae875342b32ea90fb4424be35
Ncookiez <ncukiert@gmail.com>
2024-07-09 17:33:52 -0400

    added meme vault list

--- apps/app/src/vaultLists/default/index.ts
+++ apps/app/src/vaultLists/default/index.ts
@@ -10,9 +10,9 @@ import { optimismSepoliaVaults } from './optimismSepolia'
 const defaultVaultList: VaultList = {
   name: 'Cabana Vault List',
   keywords: ['pooltogether', 'cabana', 'g9', 'optimism', 'base', 'arbitrum'],
-  version: { major: 2, minor: 8, patch: 3 },
-  timestamp: '2024-07-08T19:38:00Z',
-  logoURI: `${DOMAINS.app}/pooltogether-token-logo.svg`,
+  version: { major: 2, minor: 8, patch: 4 },
+  timestamp: '2024-07-09T21:07:28Z',
+  logoURI: `${DOMAINS.app}/favicon.png`,
   tokens: [
     ...optimismVaults,
     ...baseVaults,
