0468efe13037807e5eccc53f9642f17102a6c1d9
Ncookie <ncukiert@gmail.com>
2024-08-14 08:54:43 +0900

    added new more generic `useZapArgs` hook

---
+++ apps/app/src/hooks/zaps/useZapArgs.ts
@@ -0,0 +1,536 @@
+import { Mutable } from '@shared/types'
+import {
+  calculatePercentageOfBigInt,
+  DOLPHIN_ADDRESS,
+  getAssetsFromShares,
+  getSharesFromAssets,
+  lower,
+  NETWORK,
+  WRAPPED_NATIVE_ASSETS
+} from '@shared/utilities'
+import { useMemo } from 'react'
+import {
+  getBeefyWithdrawTx,
+  getCurveAddLiquidityTx,
+  getDepositTx,
+  getLpSwapAmountOut,
+  getRedeemTx,
+  getUnwrapTx,
+  getVelodromeAddLiquidityTx,
+  getWrapTx
+} from 'src/zapUtils'
+import { Address, ContractFunctionArgs, zeroAddress } from 'viem'
+import { useAccount } from 'wagmi'
+import { VELODROME_ADDRESSES, ZAP_SETTINGS } from '@constants/config'
+import { zapRouterABI } from '@constants/zapRouterABI'
+import { useCurveAddLiquidityOutput } from './useCurveAddLiquidityOutput'
+import { useSwapTx } from './useSwapTx'
+import { useZapTokenInfo } from './useZapTokenInfo'
+
+type ZapConfig = ContractFunctionArgs<typeof zapRouterABI, 'payable', 'executeOrder'>[0]
+type ZapRoute = Mutable<ContractFunctionArgs<typeof zapRouterABI, 'payable', 'executeOrder'>[1]>
+
+// TODO: allow for withdrawing from velodrome or curve lps when appropriate
+// TODO: allow for deposits into beefy vaults when appropriate
+/**
+ * Returns zap args
+ * @param chainId the chain ID to zap within
+ * @param inputToken the token being zapped from
+ * @param outputToken the token being zapped to
+ */
+export const useZapArgs = (
+  chainId: NETWORK,
+  inputToken: { address: Address; decimals: number; amount: bigint },
+  outputToken: { address: Address; decimals: number }
+) => {
+  const zapRouterAddress = ZAP_SETTINGS[chainId]?.zapRouter as Address | undefined
+  const wrappedNativeTokenAddress = WRAPPED_NATIVE_ASSETS[chainId]
+  const velodromeRouterAddress = VELODROME_ADDRESSES[chainId]?.router as Address | undefined
+
+  const isValidInputToken =
+    !!inputToken?.address && inputToken.decimals !== undefined && !!inputToken.amount
+  const isValidOutputToken = !!outputToken?.address && outputToken.decimals !== undefined
+
+  const { address: userAddress } = useAccount()
+
+  const { data: inputTokenInfo, isFetched: isFetchedInputTokenInfo } = useZapTokenInfo(
+    chainId,
+    inputToken?.address
+  )
+  const { data: outputTokenInfo, isFetched: isFetchedOutputTokenInfo } = useZapTokenInfo(
+    chainId,
+    outputToken?.address
+  )
+
+  const swapInputToken = useMemo(() => {
+    if (isValidInputToken) {
+      if (lower(inputToken.address) === DOLPHIN_ADDRESS && !!wrappedNativeTokenAddress) {
+        return {
+          address: wrappedNativeTokenAddress,
+          decimals: inputToken.decimals,
+          amount: inputToken.amount
+        }
+      } else if (isFetchedInputTokenInfo) {
+        if (!!inputTokenInfo.vaultToken && !!inputTokenInfo.exchangeRate) {
+          return {
+            address: inputTokenInfo.vaultToken.address,
+            decimals: inputTokenInfo.vaultToken.decimals,
+            amount: getAssetsFromShares(
+              inputToken.amount,
+              inputTokenInfo.exchangeRate,
+              inputToken.decimals
+            )
+          }
+        } else if (!!inputTokenInfo.beefyVault) {
+          return {
+            address: inputTokenInfo.beefyVault.want,
+            decimals: inputTokenInfo.beefyVault.decimals,
+            amount: getAssetsFromShares(
+              inputToken.amount,
+              inputTokenInfo.beefyVault.pricePerFullShare,
+              inputToken.decimals
+            )
+          }
+        } else {
+          return inputToken
+        }
+      }
+    }
+  }, [inputToken, isValidInputToken, inputTokenInfo, isFetchedInputTokenInfo])
+
+  const swapOutputTokens = useMemo(() => {
+    if (isValidOutputToken) {
+      if (lower(outputToken.address) === DOLPHIN_ADDRESS && !!wrappedNativeTokenAddress) {
+        return [{ address: wrappedNativeTokenAddress, decimals: outputToken.decimals }]
+      } else if (isFetchedOutputTokenInfo) {
+        if (!!outputTokenInfo.lpToken) {
+          if (outputTokenInfo.isVelodromeLp) {
+            return [
+              {
+                address: outputTokenInfo.lpToken.token0.address,
+                decimals: outputTokenInfo.lpToken.token0.decimals
+              },
+              {
+                address: outputTokenInfo.lpToken.token1.address,
+                decimals: outputTokenInfo.lpToken.token1.decimals
+              }
+            ]
+          } else if (
+            outputTokenInfo.isCurveLp &&
+            !!outputTokenInfo.lpToken.bestCurveInputTokenAddress
+          ) {
+            if (
+              lower(outputTokenInfo.lpToken.token0.address) ===
+              lower(outputTokenInfo.lpToken.bestCurveInputTokenAddress)
+            ) {
+              return [
+                {
+                  address: outputTokenInfo.lpToken.token0.address,
+                  decimals: outputTokenInfo.lpToken.token0.decimals
+                }
+              ]
+            } else if (
+              lower(outputTokenInfo.lpToken.token1.address) ===
+              lower(outputTokenInfo.lpToken.bestCurveInputTokenAddress)
+            ) {
+              return [
+                {
+                  address: outputTokenInfo.lpToken.token1.address,
+                  decimals: outputTokenInfo.lpToken.token1.decimals
+                }
+              ]
+            }
+          }
+        } else if (!!outputTokenInfo.vaultToken) {
+          return [
+            {
+              address: outputTokenInfo.vaultToken.address,
+              decimals: outputTokenInfo.vaultToken.decimals
+            }
+          ]
+        } else {
+          return [outputToken]
+        }
+      }
+    }
+
+    return []
+  }, [outputToken, isValidOutputToken, outputTokenInfo, isFetchedOutputTokenInfo])
+
+  const isSwapNecessary =
+    isValidInputToken &&
+    isValidOutputToken &&
+    !!swapInputToken &&
+    ((swapOutputTokens.length === 1 &&
+      lower(swapInputToken.address) !== lower(swapOutputTokens[0].address)) ||
+      swapOutputTokens.length === 2)
+
+  const isFirstSwapNecessary =
+    isSwapNecessary && lower(swapInputToken.address) !== lower(swapOutputTokens[0].address)
+  const isSecondSwapNecessary =
+    isSwapNecessary &&
+    swapOutputTokens.length === 2 &&
+    lower(swapInputToken.address) !== lower(swapOutputTokens[1].address)
+
+  const {
+    data: firstSwapTx,
+    isFetched: isFetchedFirstSwapTx,
+    isFetching: isFetchingFirstSwapTx
+  } = useSwapTx({
+    chainId,
+    from: {
+      address: swapInputToken?.address!,
+      decimals: swapInputToken?.decimals!,
+      amount: !!swapInputToken
+        ? swapOutputTokens.length === 2
+          ? swapInputToken.amount / 2n
+          : swapInputToken.amount
+        : 0n
+    },
+    to: swapOutputTokens[0],
+    userAddress: zapRouterAddress!,
+    options: { enabled: isFirstSwapNecessary }
+  })
+
+  const {
+    data: secondSwapTx,
+    isFetched: isFetchedSecondSwapTx,
+    isFetching: isFetchingSecondSwapTx
+  } = useSwapTx({
+    chainId,
+    from: {
+      address: swapInputToken?.address!,
+      decimals: swapInputToken?.decimals!,
+      amount: !!swapInputToken ? swapInputToken.amount / 2n : 0n
+    },
+    to: swapOutputTokens[1],
+    userAddress: zapRouterAddress!,
+    options: { enabled: isSecondSwapNecessary }
+  })
+
+  const curveAddLiquidityInput =
+    isValidInputToken &&
+    isValidOutputToken &&
+    !!outputTokenInfo.lpToken &&
+    outputTokenInfo.isCurveLp &&
+    !!swapInputToken
+      ? isSwapNecessary
+        ? firstSwapTx?.amountOut.expected
+        : swapInputToken.amount
+      : undefined
+  const { data: curveAddLiquidityOutput, isFetched: isFetchedCurveAddLiquidityOutput } =
+    useCurveAddLiquidityOutput(
+      chainId,
+      outputTokenInfo?.lpToken?.address!,
+      outputTokenInfo.lpToken!.bestCurveInputTokenAddress ===
+        outputTokenInfo.lpToken!.token0.address
+        ? [curveAddLiquidityInput ?? 0n, 0n]
+        : [0n, curveAddLiquidityInput ?? 0n],
+      { enabled: !!curveAddLiquidityInput }
+    )
+
+  const amountOut = useMemo(() => {
+    if (
+      isValidInputToken &&
+      isValidOutputToken &&
+      !!inputTokenInfo &&
+      !!outputTokenInfo &&
+      !!swapInputToken?.amount &&
+      !!swapOutputTokens.length
+    ) {
+      if (outputTokenInfo.isCurveLp) {
+        if (!!outputTokenInfo.lpToken && !!curveAddLiquidityOutput) {
+          if (!!outputTokenInfo.vaultToken && !!outputTokenInfo.exchangeRate) {
+            const expected = getSharesFromAssets(
+              curveAddLiquidityOutput,
+              outputTokenInfo.exchangeRate,
+              outputTokenInfo.vaultToken.decimals
+            )
+
+            return { expected, min: calculatePercentageOfBigInt(expected, 0.99) }
+          } else {
+            return {
+              expected: curveAddLiquidityOutput,
+              min: calculatePercentageOfBigInt(curveAddLiquidityOutput, 0.99)
+            }
+          }
+        }
+      } else if (isSwapNecessary) {
+        if (outputTokenInfo.isVelodromeLp) {
+          if (
+            !!outputTokenInfo.lpToken &&
+            (!isFirstSwapNecessary || !!firstSwapTx) &&
+            (!isSecondSwapNecessary || !!secondSwapTx)
+          ) {
+            const lpSwapAmountOut = getLpSwapAmountOut(
+              {
+                address: swapInputToken.address,
+                decimals: swapInputToken.decimals,
+                amount: swapInputToken.amount
+              },
+              outputTokenInfo.lpToken,
+              { tx: firstSwapTx, isNecessary: isFirstSwapNecessary },
+              { tx: secondSwapTx, isNecessary: isSecondSwapNecessary }
+            )
+            if (!!outputTokenInfo.vaultToken && !!outputTokenInfo.exchangeRate) {
+              return {
+                expected: getSharesFromAssets(
+                  lpSwapAmountOut.expected,
+                  outputTokenInfo.exchangeRate,
+                  outputTokenInfo.vaultToken.decimals
+                ),
+                min: getSharesFromAssets(
+                  lpSwapAmountOut.min,
+                  outputTokenInfo.exchangeRate,
+                  outputTokenInfo.vaultToken.decimals
+                )
+              }
+            } else {
+              return lpSwapAmountOut
+            }
+          }
+        } else if (!!firstSwapTx) {
+          if (!!outputTokenInfo.vaultToken && !!outputTokenInfo.exchangeRate) {
+            return {
+              expected: getSharesFromAssets(
+                firstSwapTx.amountOut.expected,
+                outputTokenInfo.exchangeRate,
+                outputTokenInfo.vaultToken.decimals
+              ),
+              min: getSharesFromAssets(
+                firstSwapTx.amountOut.min,
+                outputTokenInfo.exchangeRate,
+                outputTokenInfo.vaultToken.decimals
+              )
+            }
+          } else {
+            return firstSwapTx.amountOut
+          }
+        }
+      } else if (!!outputTokenInfo.vaultToken && !!outputTokenInfo.exchangeRate) {
+        const expected = getSharesFromAssets(
+          swapInputToken.amount,
+          outputTokenInfo.exchangeRate,
+          outputTokenInfo.vaultToken.decimals
+        )
+
+        return { expected, min: expected }
+      } else {
+        return { expected: swapInputToken.amount, min: swapInputToken.amount }
+      }
+    }
+  }, [
+    isValidInputToken,
+    isValidOutputToken,
+    inputTokenInfo,
+    outputTokenInfo,
+    swapInputToken,
+    swapOutputTokens,
+    isSwapNecessary,
+    isFirstSwapNecessary,
+    isSecondSwapNecessary,
+    firstSwapTx,
+    secondSwapTx,
+    curveAddLiquidityOutput
+  ])
+
+  const isFetched =
+    isValidInputToken &&
+    isValidOutputToken &&
+    !!zapRouterAddress &&
+    !!userAddress &&
+    isFetchedInputTokenInfo &&
+    isFetchedOutputTokenInfo &&
+    !!swapInputToken &&
+    (!isFirstSwapNecessary || (isFetchedFirstSwapTx && !!firstSwapTx)) &&
+    (!isSecondSwapNecessary || (isFetchedSecondSwapTx && !!secondSwapTx)) &&
+    !!amountOut
+
+  const isFetching = !isFetched && (isFetchingFirstSwapTx || isFetchingSecondSwapTx)
+
+  const zapArgs = useMemo((): [ZapConfig, ZapRoute] | undefined => {
+    if (isFetched) {
+      const zapInputs: ZapConfig['inputs'] = [
+        {
+          token: lower(inputToken.address) === DOLPHIN_ADDRESS ? zeroAddress : inputToken.address,
+          amount: inputToken.amount
+        }
+      ]
+
+      const zapOutputs: Mutable<ZapConfig['outputs']> = [
+        {
+          token: lower(inputToken.address) === DOLPHIN_ADDRESS ? zeroAddress : inputToken.address,
+          minOutputAmount: 0n
+        },
+        {
+          token: lower(outputToken.address) === DOLPHIN_ADDRESS ? zeroAddress : outputToken.address,
+          minOutputAmount: amountOut.min
+        }
+      ]
+
+      const addZapOutput = (newOutput: (typeof zapOutputs)[number]) => {
+        const existingOutputIndex = zapOutputs.findIndex(
+          (output) => lower(output.token) === lower(newOutput.token)
+        )
+        if (existingOutputIndex === -1) {
+          zapOutputs.push(newOutput)
+        } else if (zapOutputs[existingOutputIndex].minOutputAmount < newOutput.minOutputAmount) {
+          zapOutputs[existingOutputIndex].minOutputAmount = newOutput.minOutputAmount
+        }
+      }
+
+      if (
+        (lower(inputToken.address) === DOLPHIN_ADDRESS ||
+          lower(outputToken.address) === DOLPHIN_ADDRESS) &&
+        !!wrappedNativeTokenAddress
+      ) {
+        addZapOutput({ token: wrappedNativeTokenAddress, minOutputAmount: 0n })
+      }
+
+      if (!!inputTokenInfo.vaultToken) {
+        addZapOutput({ token: inputTokenInfo.vaultToken.address, minOutputAmount: 0n })
+      }
+
+      if (!!outputTokenInfo.vaultToken) {
+        addZapOutput({ token: outputTokenInfo.vaultToken.address, minOutputAmount: 0n })
+      }
+
+      if (!!firstSwapTx || !!secondSwapTx) {
+        addZapOutput({ token: swapInputToken.address, minOutputAmount: 0n })
+      }
+
+      if (!!firstSwapTx && !!swapOutputTokens[0]) {
+        addZapOutput({ token: swapOutputTokens[0].address, minOutputAmount: 0n })
+      }
+
+      if (!!secondSwapTx && !!swapOutputTokens[1]) {
+        addZapOutput({ token: swapOutputTokens[1].address, minOutputAmount: 0n })
+      }
+
+      if (!!inputTokenInfo.lpToken) {
+        addZapOutput({ token: inputTokenInfo.lpToken.token0.address, minOutputAmount: 0n })
+        addZapOutput({ token: inputTokenInfo.lpToken.token1.address, minOutputAmount: 0n })
+      }
+
+      if (!!outputTokenInfo.lpToken) {
+        addZapOutput({ token: outputTokenInfo.lpToken.token0.address, minOutputAmount: 0n })
+        addZapOutput({ token: outputTokenInfo.lpToken.token1.address, minOutputAmount: 0n })
+      }
+
+      const zapRoute: ZapRoute = []
+
+      if (lower(inputToken.address) === DOLPHIN_ADDRESS) {
+        zapRoute.push({
+          ...getWrapTx(chainId, inputToken.amount),
+          tokens: [{ token: zeroAddress, index: -1 }]
+        })
+      } else if (!!inputTokenInfo.vaultToken && !!inputTokenInfo.exchangeRate) {
+        zapRoute.push({
+          ...getRedeemTx(
+            inputToken.address,
+            zapRouterAddress,
+            inputToken.amount,
+            inputTokenInfo.exchangeRate,
+            inputToken.decimals
+          ),
+          tokens: [{ token: inputToken.address, index: -1 }]
+        })
+      } else if (!!inputTokenInfo.beefyVault) {
+        zapRoute.push({
+          ...getBeefyWithdrawTx(inputToken.address, inputToken.amount),
+          tokens: [{ token: inputToken.address, index: -1 }]
+        })
+      }
+
+      if (!!firstSwapTx) {
+        zapRoute.push({ ...firstSwapTx.tx, tokens: [{ token: swapInputToken.address, index: -1 }] })
+      }
+
+      if (!!secondSwapTx) {
+        zapRoute.push({
+          ...secondSwapTx.tx,
+          tokens: [{ token: swapInputToken.address, index: -1 }]
+        })
+      }
+
+      if (lower(outputToken.address) === DOLPHIN_ADDRESS && !!wrappedNativeTokenAddress) {
+        zapRoute.push({
+          ...getUnwrapTx(chainId, 0n),
+          tokens: [{ token: wrappedNativeTokenAddress, index: 4 }]
+        })
+      } else if (
+        outputTokenInfo.isCurveLp &&
+        !!outputTokenInfo.lpToken?.bestCurveInputTokenAddress
+      ) {
+        zapRoute.push({
+          ...getCurveAddLiquidityTx(outputTokenInfo.lpToken.address, [0n, 0n], 0n),
+          tokens: [
+            outputTokenInfo.lpToken.bestCurveInputTokenAddress ===
+            outputTokenInfo.lpToken.token0.address
+              ? { token: outputTokenInfo.lpToken.token0.address, index: 4 }
+              : { token: outputTokenInfo.lpToken.token1.address, index: 36 }
+          ]
+        })
+      } else if (
+        outputTokenInfo.isVelodromeLp &&
+        !!outputTokenInfo.lpToken &&
+        !!velodromeRouterAddress
+      ) {
+        const halfAmount = {
+          expected: swapInputToken.amount / 2n,
+          min: calculatePercentageOfBigInt(swapInputToken.amount / 2n, 0.99)
+        }
+
+        zapRoute.push({
+          ...getVelodromeAddLiquidityTx(
+            velodromeRouterAddress,
+            {
+              address: outputTokenInfo.lpToken.token0.address,
+              amount: firstSwapTx?.amountOut ?? halfAmount
+            },
+            {
+              address: outputTokenInfo.lpToken.token1.address,
+              amount: secondSwapTx?.amountOut ?? halfAmount
+            },
+            zapRouterAddress
+          ),
+          tokens: [
+            { token: outputTokenInfo.lpToken.token0.address, index: 100 },
+            { token: outputTokenInfo.lpToken.token1.address, index: 132 }
+          ]
+        })
+      }
+
+      if (!!outputTokenInfo.vaultToken && !!outputTokenInfo.exchangeRate) {
+        zapRoute.push({
+          ...getDepositTx(outputToken.address, zapRouterAddress),
+          tokens: [{ token: outputTokenInfo.vaultToken.address, index: 4 }]
+        })
+      }
+
+      const zapConfig: ZapConfig = {
+        inputs: zapInputs,
+        outputs: zapOutputs,
+        relay: { target: zeroAddress, value: 0n, data: '0x0' },
+        user: userAddress,
+        recipient: userAddress
+      }
+
+      return [zapConfig, zapRoute]
+    }
+  }, [
+    inputToken,
+    outputToken,
+    userAddress,
+    inputTokenInfo,
+    outputTokenInfo,
+    swapInputToken,
+    swapOutputTokens,
+    firstSwapTx,
+    secondSwapTx,
+    amountOut,
+    isFetched
+  ])
+
+  return { zapArgs, amountOut, isFetched, isFetching }
+}
---
+++ apps/app/src/hooks/zaps/useZapTokenInfo.ts
@@ -0,0 +1,53 @@
+import {
+  useSelectedVaults,
+  useVaultExchangeRate,
+  useVaultTokenData
+} from '@generationsoftware/hyperstructure-react-hooks'
+import { TokenWithSupply } from '@shared/types'
+import { lower, NETWORK } from '@shared/utilities'
+import { Address } from 'viem'
+import { useBeefyVault } from './useBeefyVault'
+import { useIsCurveLp } from './useIsCurveLp'
+import { useIsVelodromeLp } from './useIsVelodromeLp'
+import { useLpToken } from './useLpToken'
+
+export const useZapTokenInfo = (chainId: NETWORK, address: Address) => {
+  const {
+    vaults: { vaults }
+  } = useSelectedVaults()
+  const vaultsArray = Object.values(vaults).filter((v) => v.chainId === chainId)
+
+  const vault = !!address && vaultsArray.find((v) => lower(v.address) === lower(address))
+
+  const { data: vaultToken, isFetched: isFetchedVaultToken } = useVaultTokenData(vault!)
+  const { data: exchangeRate, isFetched: isFetchedExchangeRate } = useVaultExchangeRate(vault!)
+  const { data: beefyVault, isFetched: isFetchedBeefyVault } = useBeefyVault(vault!)
+
+  const potentialLpToken = !!vault ? vaultToken! : { chainId, address }
+
+  const { data: isVelodromeLp, isFetched: isFetchedIsVelodromeLp } =
+    useIsVelodromeLp(potentialLpToken)
+  const { data: isCurveLp, isFetched: isFetchedIsCurveLp } = useIsCurveLp(potentialLpToken)
+
+  const { data: lpToken, isFetched: isFetchedLpToken } = useLpToken(potentialLpToken, {
+    enabled: (isVelodromeLp || isCurveLp) ?? false
+  })
+
+  const data: Partial<{
+    vaultToken: TokenWithSupply
+    exchangeRate: bigint
+    beefyVault: ReturnType<typeof useBeefyVault>['data']
+    isVelodromeLp: boolean
+    isCurveLp: boolean
+    lpToken: ReturnType<typeof useLpToken>['data']
+  }> = { vaultToken, exchangeRate, beefyVault, isVelodromeLp, isCurveLp, lpToken }
+
+  const isFetched =
+    !!chainId &&
+    !!address &&
+    (!vault || (isFetchedVaultToken && isFetchedExchangeRate && isFetchedBeefyVault)) &&
+    (!vaultToken || (isFetchedIsVelodromeLp && isFetchedIsCurveLp)) &&
+    ((!isVelodromeLp && !isCurveLp) || isFetchedLpToken)
+
+  return { data, isFetched }
+}
