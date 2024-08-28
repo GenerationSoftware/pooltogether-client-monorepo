bbbd75049e63eabad21c3bb942e1ae24aec922b0
Ncookiez <ncukiert@gmail.com>
2024-08-28 16:03:11 -0400

    added mainnet to wallet stats worker

--- workers/wallet-stats-worker/src/bindings.d.ts
+++ workers/wallet-stats-worker/src/bindings.d.ts
@@ -1,7 +1,9 @@
 export {}
+
 declare global {
   const DEPOSITS: KVNamespace
   const TOKEN_PRICES: ServiceWorkerGlobalScope
+  const MAINNET_RPC_URL: string
   const OPTIMISM_RPC_URL: string
   const BASE_RPC_URL: string
   const ARBITRUM_RPC_URL: string
