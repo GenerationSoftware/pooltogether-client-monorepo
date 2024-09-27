534655b93fcc7ea983d9f3f74353781923e8434c
Ncookiez <ncukiert@gmail.com>
2024-09-27 18:31:54 -0400

    resetting all forms when changing network

--- apps/vault-factory/src/components/forms/NetworkForm.tsx
+++ apps/vault-factory/src/components/forms/NetworkForm.tsx
@@ -1,8 +1,27 @@
 import classNames from 'classnames'
-import { useAtom } from 'jotai'
+import { useAtom, useSetAtom } from 'jotai'
 import { useEffect } from 'react'
 import { FormProvider, useForm } from 'react-hook-form'
-import { vaultChainIdAtom } from 'src/atoms'
+import {
+  contributionAmountAtom,
+  isUsingCustomYieldSourceAtom,
+  liquidationPairAddressAtom,
+  liquidationPairSmoothingFactorAtom,
+  liquidationPairTargetAuctionPeriodAtom,
+  liquidationPairTargetAuctionPriceAtom,
+  vaultAddressAtom,
+  vaultChainIdAtom,
+  vaultClaimerAddressAtom,
+  vaultFeePercentageAtom,
+  vaultFeeRecipientAddressAtom,
+  vaultNameAtom,
+  vaultOwnerAddressAtom,
+  vaultSymbolAtom,
+  vaultYieldBufferAtom,
+  vaultYieldSourceAddressAtom,
+  vaultYieldSourceIdAtom,
+  vaultYieldSourceNameAtom
+} from 'src/atoms'
 import { NextButton } from '@components/buttons/NextButton'
 import { SUPPORTED_NETWORKS } from '@constants/config'
 import { useVaultCreationSteps } from '@hooks/useVaultCreationSteps'
@@ -25,13 +44,55 @@ export const NetworkForm = (props: NetworkFormProps) => {
 
   const { nextStep } = useVaultCreationSteps()
 
+  const setVaultYieldSourceId = useSetAtom(vaultYieldSourceIdAtom)
+  const setVaultYieldSourceName = useSetAtom(vaultYieldSourceNameAtom)
+  const setVaultYieldSourceAddress = useSetAtom(vaultYieldSourceAddressAtom)
+  const setIsUsingCustomYieldSource = useSetAtom(isUsingCustomYieldSourceAtom)
+  const setVaultFeePercentage = useSetAtom(vaultFeePercentageAtom)
+  const setVaultFeeRecipientAddress = useSetAtom(vaultFeeRecipientAddressAtom)
+  const setVaultOwnerAddress = useSetAtom(vaultOwnerAddressAtom)
+  const setVaultName = useSetAtom(vaultNameAtom)
+  const setVaultSymbol = useSetAtom(vaultSymbolAtom)
+  const setVaultClaimerAddress = useSetAtom(vaultClaimerAddressAtom)
+  const setVaultYieldBuffer = useSetAtom(vaultYieldBufferAtom)
+  const setVaultAddress = useSetAtom(vaultAddressAtom)
+  const setLiquidationPairTargetAuctionPeriod = useSetAtom(liquidationPairTargetAuctionPeriodAtom)
+  const setLiquidationPairTargetAuctionPrice = useSetAtom(liquidationPairTargetAuctionPriceAtom)
+  const setLiquidationPairSmoothingFactor = useSetAtom(liquidationPairSmoothingFactorAtom)
+  const setLiquidationPairAddress = useSetAtom(liquidationPairAddressAtom)
+  const setContributionAmount = useSetAtom(contributionAmountAtom)
+
+  const resetAllFormAtoms = () => {
+    setVaultYieldSourceId('')
+    setVaultYieldSourceName('')
+    setVaultYieldSourceAddress(undefined)
+    setIsUsingCustomYieldSource(false)
+    setVaultFeePercentage(undefined)
+    setVaultFeeRecipientAddress(undefined)
+    setVaultOwnerAddress(undefined)
+    setVaultName(undefined)
+    setVaultSymbol(undefined)
+    setVaultClaimerAddress(undefined)
+    setVaultYieldBuffer(undefined)
+    setVaultAddress(undefined)
+    setLiquidationPairTargetAuctionPeriod(undefined)
+    setLiquidationPairTargetAuctionPrice(undefined)
+    setLiquidationPairSmoothingFactor(0.5)
+    setLiquidationPairAddress(undefined)
+    setContributionAmount(undefined)
+  }
+
   useEffect(() => {
     !!vaultChainId &&
       formMethods.setValue('vaultChainId', vaultChainId.toString(), { shouldValidate: true })
   }, [])
 
   const onSubmit = (data: NetworkFormValues) => {
-    setVaultChainId(parseInt(data.vaultChainId))
+    const selectedChainId = parseInt(data.vaultChainId)
+
+    if (selectedChainId !== vaultChainId) resetAllFormAtoms()
+
+    setVaultChainId(selectedChainId)
     nextStep()
   }
 
